bc3828ba2b2d9df0625d5f0c38dcfd4e32b9accc
Bashir Nasir Sani <bashnasir@cloud9.ng>
2020-05-09 14:40:48 +0100

    created sign up logic for user

---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require ('express').Router();
+const {signUp,logIn} = require('../controllers/auth.js');
+
+router.post('/api/v1/signup', signUp);
+// router.post('/api/v1/login', logIn);
+
+module.exports = router;
